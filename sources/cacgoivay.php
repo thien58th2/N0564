ebfda4e3bfad0bbd76f7fb5ad073318d46cd3066
trương thanh thiện <38430722+thien58th2@users.noreply.github.com>
2019-01-02 22:32:29 +0700

    new

--- sources/cacgoivay.php
+++ sources/cacgoivay.php
@@ -1,17 +1,22 @@
 <?php include './include/header.php'; ?>
     <main>
-    <div class="container-fluid" style=" height: 10cm;background-color:snow;border:solid 1px">
+    <div class="goivay">
+    <div class="cacgoivay">
+   
+    
 	<div class="row" style="margin-left:5cm;">
-		<div class="col-md-6" style="margin-top:2.5cm; ">
+		<div class="col-md-6" style="margin-top:4cm; ">
         <a href="chovaytheongay.php">
         <img src="./style/image/vay-tra-gop.png" alt="" width="237" height="238">
         </a>
 		</div>
-		<div class="col-md-6" style="margin-top:2.5cm;" ><a href="goivayshk.php">
+		<div class="col-md-6" style="margin-top:4cm;" ><a href="goivayshk.php">
         <img src="./style/image/so-hk.png" alt="" width="237" height="238">
         </a>
 		</div>
 	</div>
+</div>
+</div>
 </div>
     </main>
 <?php include './include/footer.php'; ?>
